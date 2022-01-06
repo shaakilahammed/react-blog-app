e8557a222be37d60702f3ea13c053264bb0db242
shaakilahammed <shaakilahammed@gmail.com>
2022-01-07 00:30:10 +0600

    Blog Home Added

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => (
+  <header className="home-header">
+    <h2>Welcome to</h2>
+    <h1>
+      "<span>My Personal Blog Site</span>"
+    </h1>
+    <p>awesome place to entertained through daily updates</p>
+  </header>
+);
+
+export default Header;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import BlogList from '../../components/Home/BlogList';
+import Header from '../../components/Home/Header';
+import SearchBar from '../../components/Home/SearchBar';
+import { blogList } from '../../config/blogList';
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Header />
+      <SearchBar />
+      <BlogList blogs={blogList} />
     </div>
   );
 };
